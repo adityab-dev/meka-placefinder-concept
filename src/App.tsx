630c99669c09e0bc1bc22e616700db796dff151e
adityab-dev <120619189+adityab-dev@users.noreply.github.com>
2023-02-03 19:15:52 +0530

    modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import Filters from "./Components/Filters/Filters";
+import PropertyList from "./Components/PropertyList/PropertyList";
+import Sidebar from "./Components/Sidebar/Sidebar";
+
+import "./App.css";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <main className="app-container flex">
+      <div className="sidebar-container">
+        <Sidebar />
+      </div>
+      <div className="filters-container">
+        <Filters />
+      </div>
+      <div className="property-contianer">
+        <PropertyList />
+      </div>
+    </main>
+  );
 }
 
 export default App;
