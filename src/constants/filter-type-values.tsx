72726b0fa8e14e95b4dd15257d6555ffc26c0513
adityab-dev <120619189+adityab-dev@users.noreply.github.com>
2023-02-06 19:29:50 +0530

    store for constants

---
+++ src/constants/filter-type-values.tsx
@@ -0,0 +1,15 @@
+const enum typesFilterValues {
+  all = "all",
+  building = "building",
+  apartment = "apartment",
+  office = "office",
+  shop = "shop",
+  house = "house",
+}
+
+export const all = typesFilterValues.all;
+export const building = typesFilterValues.building;
+export const apartment = typesFilterValues.apartment;
+export const office = typesFilterValues.office;
+export const shop = typesFilterValues.shop;
+export const house = typesFilterValues.house;
---
+++ src/constants/input-names.ts
@@ -0,0 +1,11 @@
+const enum Names {
+  type = "type",
+  features = "features",
+  style = "style",
+  price = "price",
+}
+
+export const type = Names.type;
+export const features = Names.features;
+export const style = Names.style;
+export const price = Names.price;
